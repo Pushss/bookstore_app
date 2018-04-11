df51fa145049903b7735305b87e9851f626ee49f
Pushss <chrisjamescam10@gmail.com>
2018-04-11 17:19:59 +0100

    update, delete and search functions added to back end

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import sqlite3
 def connect():
     conn=sqlite3.connect("books.db")
     cur=conn.cursor() #create cursor within db
-    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY,title text,author text,year integer,isbn integer)")
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY AUTOINCREMENT,title text,author text,year integer,isbn integer)")
     conn.commit()
     conn.close()
 
@@ -22,9 +22,31 @@ def view():
     conn.close()
     return rows
 
+def search_entry(title="",author="",year="",isbn=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR ISBN =?",(title,author,year,isbn))
+    rows=cur.fetchall()
+    conn.commit()
+    conn.close()
+
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+def update(id,title,author,year,isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))
+    conn.commit()
+    conn.close()
+
 
 #("UPDATE book SET title=?, author=?, year=?, WHERE ISBN =?",(title,author,year,isbn))
 
 connect()
-insert("the sea", "john tablet", 3342,32323)
+#print(search_entry(year='3342'))
 print(view())
